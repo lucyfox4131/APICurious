ba2556cbf803e36260c3e3ff64bc7685aaf05d21
Lucy Fox <lucyreneefox@gmail.com>
2016-07-11 17:08:29 -0600

    Login and logout with github totally set up and functioning. Navbar displays different links based on if user is logged in or out

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  def self.from_omniauth(auth_info)
+    where(uid: auth_info[:uid]).first_or_create do |new_user|
+      new_user.uid = auth_info.uid
+      new_user.name = auth_info.info.name
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
   get 'homes/show'
 
   get '/auth/github', as: :github_login
   get 'auth/github/callback', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: :logout
   root to: 'homes#show'
 end
