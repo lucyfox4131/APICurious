77c02ddecac83b59fd671a5f26c2f4151168b264
Lucy Fox <lucyreneefox@gmail.com>
2016-07-11 17:15:06 -0600

    add gems to gemfile for testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,13 @@ require 'capybara/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
